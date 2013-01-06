137a544aa9368be61f34c4a6444c0510fee93f0e
Malte Schmitz <malte@schmitz-sh.de>
2013-01-06 18:37:12 +0100

    Switches to OOP version of string library.

--- adif.lua
+++ adif.lua
@@ -7,16 +7,16 @@ adif = {}
 -- comment - set to true if you want to get first lines as comment field
 local function fields(t, ts, te, comment)
   local result = {}
-  local s, e, field, length = string.find(t, "<([%w_]+):(%d+)>", ts)
+  local s, e, field, length = t:find("<([%w_]+):(%d+)>", ts)
   if comment then
-    local c = string.sub(t, ts, math.min(s - 1, te))
-    if not string.find(c, "^%s*$") then
+    local c = t:sub(ts, math.min(s - 1, te))
+    if not c:find("^%s*$") then
       result.comment = c
     end
   end
   while s and e < te do
-    result[field] = string.sub(t, e + 1, math.min(e + length, te))
-    s, e, field, length = string.find(t, "<([%w_]+):(%d+)>", e + length + 1)
+    result[field] = t:sub(e + 1, math.min(e + length, te))
+    s, e, field, length = t:find("<([%w_]+):(%d+)>", e + length + 1)
   end
   return result
 end
@@ -31,7 +31,7 @@ local function text(f, t, comment)
     t.comment = nil
   end
   for k,v in pairs(t) do
-    f:write("<", k, ":", string.len(v), ">", v, "\n")
+    f:write("<", k, ":", v:len(), ">", v, "\n")
   end
 end
 
@@ -47,18 +47,18 @@ function adif:read(filename)
   setmetatable(result, self)
   
   -- read header fields
-  local s, e = string.find(t, "<eoh>")
+  local s, e = t:find("<eoh>")
   if s then
     result.header = fields(t, 1, s - 1, true)
   end
 
   -- read qso fields
   local old = e + 1
-  s, e = string.find(t, "<eor>", old)
+  s, e = t:find("<eor>", old)
   while s do
     table.insert(result.data, fields(t, old, s - 1))
     old = e + 1
-    s, e = string.find(t, "<eor>", old)
+    s, e = t:find("<eor>", old)
   end
   
   return result
