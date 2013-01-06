f670b769555298784b072f4436725e37c4574096
Malte Schmitz <malte@schmitz-sh.de>
2013-01-06 20:43:15 +0100

    Adds background color experiment to excel test

--- excel.lua
+++ excel.lua
@@ -4,6 +4,8 @@ excel.Visible = true
 wb = excel.Workbooks:Add()
 ws = wb.Worksheets(1)
 
-for i=1, 20 do
+for i = 1, 56 do
     ws.Cells(i,1).Value2 = i
+    ws.Cells(i,1).Interior.ColorIndex = i
+    ws.Cells(i,1).Interior.Pattern = 1
 end
